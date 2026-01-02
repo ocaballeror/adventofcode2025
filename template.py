70529d4deaa143d60517b379f4f0e635a1a69a3c
Oscar Caballero <ocaballeror@tutanota.com>
2026-01-02 15:48:12 +0100

    Add template

---
+++ template.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def read_input():
+    with open(sys.argv[1] if len(sys.argv) > 1 else "input") as f:
+        pass
+
+    return []
+
+
+def part1():
+    data = read_input()
+    return 0
+
+
+def part2():
+    data = read_input()
+    return 0
+
+
+if __name__ == "__main__":
+    print("Part 1:", part1())
+    print("Part 2:", part2())
